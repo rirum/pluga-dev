d8bb6a4fa1a2146ac39fc2ea9d19f02c6d0f8393
camille <106037608+rirum@users.noreply.github.com>
2023-12-18 01:42:48 -0300

    feat: pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,9 @@ export default function Home() {
   const [toolsData, setToolsData] = useState<Array<{ id: number }>>([]);
   const [filteredTools, setFilteredTools] = useState<Array<{ id: number }>>([]);
   const [loading, setLoading]= useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 12;
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,20 +34,38 @@ export default function Home() {
     fetchData();
   }, []);
 
+  const totalItems = filteredTools.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentTools = filteredTools.slice(startIndex, endIndex);
+
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
 
   return (
     <>
-    <Background>
-      <SearchBar></SearchBar>
-      <CardsWrapper>
-      {loading ? (
+     <Background>
+        <SearchBar></SearchBar>
+        <CardsWrapper>
+          {loading ? (
             <p>Loading...</p>
           ) : (
-            filteredTools.map((tool) => <Card key={tool.id} tool={tool} />)
+            currentTools.map((tool) => <Card key={tool.id} tool={tool} />)
           )}
-      </CardsWrapper>
-  
-    </Background>
+        </CardsWrapper>
+        <Pagination>
+          <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
+            Previous
+          </button>
+          <span>{`Page ${currentPage} of ${totalPages}`}</span>
+          <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+            Next
+          </button>
+        </Pagination>
+      </Background>
    
     </>
   )
@@ -62,11 +83,32 @@ align-items: center;
 
 const CardsWrapper = styled.div`
 width: 1190px;
-height: 300px;
+height: auto;
 display: flex;
 flex-wrap: wrap;
-
-
-
+margin-bottom: 40px;
  
 `
+
+const Pagination = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 50px;
+  bottom: 0;
+  button {
+    cursor: pointer;
+    padding: 8px 16px;
+    font-size: 14px;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    margin: 0 8px;
+
+    &:disabled {
+      background-color: #ccc;
+      cursor: not-allowed;
+    }
+  }
+`;
