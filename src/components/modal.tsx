20eb25a90670475c52d3d5b8a43656b8873bdf65
camille <106037608+rirum@users.noreply.github.com>
2023-12-19 18:02:13 -0300

    feat: modal style

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,41 +1,101 @@
-import styled from "styled-components";
-import { Tool } from "./search";
+import styled from 'styled-components';
+import { Tool } from './search';
 
 type ModalProps = {
-    tool: Tool; 
-    onClose: () => void;
-  };
+  tool: Tool;
+  onClose: () => void;
+};
 
-  const Modal: React.FC<ModalProps> = ({ tool, onClose }) => {
-    return (
-      <ModalOverlay>
-        <ModalContent>
-          <h2>{tool.name}</h2>
-          <p>Icon: {tool.icon}</p>
-        
-          <button onClick={onClose}>Fechar</button>
-        </ModalContent>
-      </ModalOverlay>
-    );
-  };
-  
-  const ModalOverlay = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-  
-  const ModalContent = styled.div`
-    background: white;
-    padding: 20px;
-    border-radius: 5px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-  `;
-  
-  export default Modal;
+const Modal: React.FC<ModalProps> = ({ tool, onClose }) => {
+  return (
+    <ModalOverlay>
+      <ModalContent>
+        <ChosenApp>
+          <AppLogo color={tool.color}>
+            <img src={tool.icon} alt={tool.name}></img>
+          </AppLogo>
+          <AppInfo>
+            <h2>{tool.name}</h2>
+            <button>ACESSAR</button>
+          </AppInfo>
+        </ChosenApp>
+        <LastSeenApps>
+          <p>ÚLTIMAS FERRAMENTAS VISUALIZADAS</p>
+        </LastSeenApps>
+
+        <button onClick={onClose}>Fechar</button>
+      </ModalContent>
+    </ModalOverlay>
+  );
+};
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalContent = styled.div`
+  background: white;
+  width: 800px;
+  height: 600px;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+`;
+
+const ChosenApp = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: pink;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AppLogo = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 100px;
+  background-color: ${(props) => props.color || '#EFEFEF'};
+  margin-top: 30px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 70%;
+    height: 70%;
+    object-fit: contain;
+  }
+`;
+
+const AppInfo = styled.div`
+  margin-left: 40px;
+  h2 {
+    width: 300px;
+    height: 40px;
+    background-color: red;
+    font-size: 20px;
+  }
+  button {
+    width: 200px;
+    height: 40px;
+    border-radius: 40px;
+    margin-top: 15px;
+    border: none;
+  }
+`;
+
+const LastSeenApps = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: paleturquoise;
+`;
+export default Modal;
--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,109 +1,130 @@
-import styled from "styled-components";
-import { FaSearch } from "react-icons/fa";
-import { ChangeEvent, useState, MouseEvent } from "react";
+import styled from 'styled-components';
+import { FaSearch } from 'react-icons/fa';
+import { ChangeEvent, useState, MouseEvent } from 'react';
 
 export type Tool = {
-    id: number;
-    icon?: string;
-    name?: string;
+  id: number;
+  icon?: string;
+  name?: string;
+  color?: string;
+};
+
+type SearchBarProps = {
+  tools: Tool[];
+  onSearch: (searchTerm: string) => void;
+};
+
+export default function SearchBar({ tools, onSearch }: SearchBarProps) {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [listSearch, setListSearch] = useState<Tool[]>([]);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+
+    if (value.length >= 3) {
+      const list = tools.filter((tool) =>
+        tool.name?.toLowerCase().includes(value.toLowerCase())
+      );
+      setListSearch(list);
+      onSearch(value);
+    } else {
+      setListSearch([]);
+    }
   };
-  
-  type SearchBarProps = {
-    tools: Tool[];
-    onSearch: (searchTerm: string) => void;
+
+  const handleSearch = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if (onSearch && searchTerm.length >= 3) {
+      onSearch(searchTerm);
+    }
   };
-  
 
-export default function SearchBar({tools, onSearch}: SearchBarProps){
-    const [searchTerm, setSearchTerm] = useState<string>("");
-    const [listSearch, setListSearch] = useState<Tool[]>([]);
-
-    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const value = event.target.value
-        setSearchTerm(value);
-
-        if(value.length >= 3){
-            const list = tools.filter(tool => tool.name?.toLowerCase().includes(value.toLowerCase()))
-            setListSearch(list)
-            onSearch(value)
-        }else{
-            setListSearch([])
-        }
+  const handleKey = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      onSearch(searchTerm);
     }
+  };
 
-    const handleSearch = (event: MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-        if (onSearch && searchTerm.length >= 3){
-            onSearch(searchTerm);
-        }
-    }
+  return (
+    <div style={{ position: 'relative' }}>
+      <StyledSearch>
+        <button onClick={handleSearch}>
+          <StyledFaSearch />
+        </button>
 
-    const handleKey = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if(event.key === 'Enter'){
-            onSearch(searchTerm);
-        }
-
-    }
-
-    return(
-        
-        <div style={{position: 'relative'}}>
-        <StyledSearch >
-            <button onClick={handleSearch}>
-            <StyledFaSearch/>
-            </button>
-       
-             <input 
-             placeholder="BUSCA FERRAMENTA"
-             value={searchTerm}
-             onChange={handleInputChange}
-             onKeyDown={handleKey}></input>
-        </StyledSearch>
-        <ul style={{listStyleType: 'none', backgroundColor: 'white', zIndex:'1000', position: 'absolute', left: '0', top: '100%', width: '100%'}}>
+        <input
+          placeholder="BUSCA FERRAMENTA"
+          value={searchTerm}
+          onChange={handleInputChange}
+          onKeyDown={handleKey}
+        ></input>
+      </StyledSearch>
+      <ul
+        style={{
+          listStyleType: 'none',
+          backgroundColor: 'white',
+          zIndex: '1000',
+          position: 'absolute',
+          left: '0',
+          top: '100%',
+          width: '100%',
+        }}
+      >
         {listSearch.map((tool) => (
-          <li key={tool.id} style={{display: 'flex', alignItems:'flex-start', marginRight: '50px', marginTop:'20px'}}>
-            <img src={tool.icon} alt={tool.name} style={{width: '20px', height:'20px', marginRight: '5px' }}></img>
+          <li
+            key={tool.id}
+            style={{
+              display: 'flex',
+              alignItems: 'flex-start',
+              marginRight: '50px',
+              marginTop: '20px',
+            }}
+          >
+            <img
+              src={tool.icon}
+              alt={tool.name}
+              style={{ width: '20px', height: '20px', marginRight: '5px' }}
+            ></img>
             {tool.name}
-          </li>  
+          </li>
         ))}
-        </ul>
-        </div>
-        
-        
-    )
+      </ul>
+    </div>
+  );
 }
 
 const StyledSearch = styled.div`
-   width: 1260px;
-    margin: 0 auto;
-    margin-top: 10px;
-    background-color: #DDD;
-    padding-left: 50px;
-    border-radius: 5px;
-    justify-content: center;
-  
-        input{
-            height: 50px;   
-            background-color: #DDD;
-            box-shadow: 0 0 0 0;
-            border: 0 none;
-            outline: 0;
-          
-            ::placeholder{
-                    font-size: 40px;
-                    color: #707070;
-                }
-        }  
-        button{
-            background-color: transparent;
-            box-shadow: 0 0 0 0;
-            border: 0 none;
-            outline: 0;
-            cursor: pointer;
-        }
-`
+  width: 1260px;
+  margin: 0 auto;
+  margin-top: 10px;
+  background-color: #ddd;
+  padding-left: 50px;
+  border-radius: 5px;
+  justify-content: center;
+
+  input {
+    height: 50px;
+    background-color: #ddd;
+    box-shadow: 0 0 0 0;
+    border: 0 none;
+    outline: 0;
+
+    ::placeholder {
+      font-size: 40px;
+      color: #707070;
+    }
+  }
+  button {
+    background-color: transparent;
+    box-shadow: 0 0 0 0;
+    border: 0 none;
+    outline: 0;
+    cursor: pointer;
+  }
+`;
 
 const StyledFaSearch = styled(FaSearch)`
-    margin-right: 10px;
-    font-size:15px;
-`
+  margin-right: 10px;
+  font-size: 15px;
+`;
